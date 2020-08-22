03d2ee56896028aa026a4618ee56e66baec8096d
luancunha <luanport15@gmail.com>
2020-08-21 21:36:37 -0300

    Alteracao nos graficos

--- proj/views.py
+++ proj/views.py
@@ -7,6 +7,7 @@ def index(request):
     reg = registro.objects.all()
     doe = doenca.objects.all()
     cid = cidade.objects.all()
+    hos = hospital.objects.all()
     
     def sql():
         cursor = connection.cursor()
@@ -18,12 +19,12 @@ def index(request):
     
     def sql2():
         cursor = connection.cursor()
-        cursor.execute("SELECT hospital,SUM(ncasos) FROM proj_registro GRoUP BY hospital;")
+        cursor.execute("SELECT c.nome,SUM(r.ncasos) FROM proj_registro as R INNER JOIN  proj_hospital as H ON r.hospital=h.idhospital INNER JOIN proj_cidade as C ON h.cidade=c.idcidade GRoUP BY c.nome ORDER BY c.idcidade")
         row = cursor.fetchall()
         return row
     
     row2 = sql2()
     
-    context={'doe':doe, 'reg':reg, 'row':row, 'row2':row2, 'cid':cid}
+    context={'doe':doe, 'reg':reg, 'row':row, 'row2':row2, 'cid':cid, 'hos':hos}
 
     return render(request,'index.html',context)
